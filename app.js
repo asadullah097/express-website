aa424349007b4ec05c00867c0c7262a8b8aa7c7b
Asad ullah <38780235+asadullah097@users.noreply.github.com>
2020-11-07 07:57:28 +0500

    jade engine view

--- app.js
+++ app.js
@@ -5,18 +5,26 @@ const nodemailer = require('nodemailer');
 const { urlencoded } = require("body-parser");
 
 const app=express();
-
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 //middleware
 
 app.use(bodyParser.json());
 
 app.use(urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //route 
 app.get('/',(req,res)=>{
-    res.send('Hello world!');
+    res.render('index',{title: 'Welcome'});
+});
+app.get('/about',(req,res)=>{
+    res.render('about',{title: 'about'});
 });
 
+app.get('/contact',(req,res)=>{
+    res.render('contact',{title: 'contact'});
+});
 //server listen
 
 app.listen(3000);
