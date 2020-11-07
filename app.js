910f5b9d6330214ca1c2788b10fac8236d403cbc
Asad ullah <38780235+asadullah097@users.noreply.github.com>
2020-11-07 08:08:38 +0500

    final commit

--- app.js
+++ app.js
@@ -25,6 +25,33 @@ app.get('/about',(req,res)=>{
 app.get('/contact',(req,res)=>{
     res.render('contact',{title: 'contact'});
 });
+app.post('/contact/send', function(req, res){
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'your@email.com',
+			pass: 'password'
+		}
+	});
+
+	var mailOptions = {
+		from: 'Brad Traversy <techguyinfo@gmail.com>',
+		to: 'support@joomdigi.com',
+		subject: 'Website Submission',
+		text: 'You have a submission with the following details... Name: '+req.body.name+'Email: '+req.body.email+ 'Message: '+req.body.message,
+		html: '<p>You have a submission with the following details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.redirect('/');
+		} else {
+			console.log('Message Sent: '+info.response);
+			res.redirect('/');
+		}
+    });
+});
 //server listen
 
 app.listen(3000);
